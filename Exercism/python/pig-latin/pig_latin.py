45ee2d9f16fc62ddb7b1be648af57a9f98aef199
Krishna Kumar <kk3271038@gmail.com>
2026-07-26 01:55:01 +0530

    Pig latin Problem

---
+++ Exercism/python/pig-latin/pig_latin.py
@@ -0,0 +1,21 @@
+def translate(text):
+    words = text.split()
+    result = []
+
+    for word in words:
+        if word.startswith(("a", "e", "i", "o", "u", "xr", "yt")):
+            result.append(word + "ay")
+            continue
+
+        i = 0
+        while i < len(word):
+            if word[i] in "aeiou":
+                break
+            if word[i] == "y" and i != 0:
+                break
+            if word[i:i+2] == "qu":
+                i += 2
+                break
+            i += 1
+        result.append(word[i:] + word[:i] + "ay")
+    return " ".join(result)
