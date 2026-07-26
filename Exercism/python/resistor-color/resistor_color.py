70fc56cbfe43c83b9b02c4a53590234f00fc50db
Krishna Kumar <kk3271038@gmail.com>
2026-07-27 00:04:16 +0530

    Resistor colour Problem

---
+++ Exercism/python/resistor-color/resistor_color.py
@@ -0,0 +1,7 @@
+def color_code(color):
+    
+    COLORS = ["black", "brown", "red", "orange", "yellow","green", "blue", "violet", "grey", "white"]
+    return COLORS.index(color)
+
+def colors():
+    return ["black", "brown", "red", "orange", "yellow","green", "blue", "violet", "grey", "white"]
