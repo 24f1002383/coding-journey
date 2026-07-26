08313decd3633252d922b125aa119e4b87854df3
Krishna Kumar <kk3271038@gmail.com>
2026-07-26 23:49:35 +0530

    Rotational Cipher

---
+++ Exercism/python/rotational-cipher/rotational_cipher.py
@@ -0,0 +1,13 @@
+def rotate(text, key):
+    
+    result = ""
+
+    for ch in text:
+        if 'a' <= ch <= 'z':
+            result += chr((ord(ch) - 97 + key) % 26 + 97)
+        elif 'A' <= ch <= 'Z':
+            result += chr((ord(ch) - 65 + key) % 26 + 65)
+        else:
+            result += ch
+
+    return result
