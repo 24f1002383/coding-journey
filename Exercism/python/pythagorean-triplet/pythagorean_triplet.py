7720398bfeebc77d06f22304b10956bd4ccf9c93
Krishna Kumar <kk3271038@gmail.com>
2026-07-27 23:32:15 +0530

    Best Apparoach to get pythagorean triplet for me

--- Exercism/python/pythagorean-triplet/pythagorean_triplet.py
+++ Exercism/python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,13 +1,22 @@
+import math
 def triplets_with_sum(number):
+
+    if number <= 0 or number % 2 != 0:
+        return []
+
     result = []
-    for a in range(1, number // 3 + 1):
-        x = 2 * (number - a)
-        y = number * (number - 2 * a)
-        if x <= 0 or y <= 0 or y % x != 0:
-            continue
-        b = y // x
-        c = number - a - b
-        if a < b < c:
-            result.append([a, b, c])
-            
+    m = 2
+    while 2 * m * (m + 1) <= number:
+        for n in range(1, m):
+            if (m - n) % 2 == 1 and math.gcd(m, n) == 1:
+                perimeter_unit = 2 * m * (m + n)
+                if number % perimeter_unit == 0:
+                    k = number // perimeter_unit
+                    a = k * (m * m - n * n)
+                    b = k * (2 * m * n)
+                    c = k * (m * m + n * n)
+                    if a > b:
+                        a, b = b, a
+                    result.append([a, b, c])
+        m += 1
     return result
