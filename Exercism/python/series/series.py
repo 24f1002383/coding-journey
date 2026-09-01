96043cdf3cf8482a0ef903a8dc7d2bb3335f078f
24f1002383 <24f1002383@ds.study.iitm.ac.in>
2026-09-01 22:09:53 +0530

    Series Problem

---
+++ Exercism/python/series/series.py
@@ -0,0 +1,11 @@
+def slices(series, length):
+    
+    if length == 0:
+        raise ValueError("slice length cannot be zero")
+    elif length < 0:
+        raise ValueError("slice length cannot be negative")
+    if len(series) == 0:
+        raise ValueError("series cannot be empty")
+    if len(series) < length:
+        raise ValueError("slice length cannot be greater than series length")
+    return [series[i:i + length] for i in range(len(series) - length + 1)]
